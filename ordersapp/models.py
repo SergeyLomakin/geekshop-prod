f8f119520b01f054eb59eefac7e48cabf270bbbf
SergeyLomakin <sergeylomakinals@mail.ru>
2021-06-29 16:43:03 +0500

    created models for ordersapp

--- ordersapp/models.py
+++ ordersapp/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from mainapp.models import Product
+
+
+class Order(models.Model):
+    FORMING = 'FM'
+    SENT_TO_PROCESS = 'STP'
+    PROCEEDED = 'PRD'
+    PAID = 'PD'
+    READY = 'RD'
+    CANCEL = 'CNC'
+    DELIVERED = 'DVD'
+
+    STATUSES = (
+        (FORMING, 'Формаируется'),
+        (SENT_TO_PROCESS, 'Отправлен в обработку'),
+        (PROCEEDED, 'Обработан'),
+        (PAID, 'Оплачек'),
+        (READY, 'Готов к выдаче'),
+        (CANCEL, 'Отменён'),
+        (DELIVERED, 'Выдан'),
+    )
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE())
+
+    created = models.DateTimeField(auto_now_add=True, verbose_name='создан')
+    updated = models.DateTimeField(auto_now=True, verbose_name='обнавлён')
+    is_active = models.BooleanField(default=True)
+
+    status = models.CharField(choices=STATUSES, max_length=3, default=FORMING, verbose_name='статус')
+
+    def _get_total_quantity(self):
+        _items = self.orderitems.select_related()
+        _totalquantity = sum(list(map(lambda x: x.quantity, _items)))
+        return _totalquantity
+
+    def _get_total_cost(self):
+        _items = self.orderitems.select_related()
+        _totalcost = sum(list(map(lambda x: x.get_product_cost(), _items)))
+        return _totalcost
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='orderitems')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='продукт')
+    quantity = models.PositiveSmallIntegerField(default=0, verbose_name='количество')
+
+    def get_product_cost(self):
+        return self.product.price * self.quantity
