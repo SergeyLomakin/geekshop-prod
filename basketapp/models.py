a2e8240f78a2455e11eeacf2658cf0c0b2c94601
SergeyLomakin <sergeylomakinals@mail.ru>
2021-07-07 17:31:47 +0500

    add signals

--- basketapp/models.py
+++ basketapp/models.py
@@ -32,3 +32,7 @@ class Basket(models.Model):
         return _totalcost
         
     total_cost = property(_get_total_cost)
+
+    @staticmethod
+    def get_item(pk):
+        return Basket.objects.get(pk=pk)
--- ordersapp/views.py
+++ ordersapp/views.py
@@ -2,8 +2,10 @@ from django.views.generic import ListView, CreateView, DeleteView, UpdateView, D
 from django.urls import reverse_lazy, reverse
 from django.forms import inlineformset_factory
 from django.db import transaction
+from django.db.models.signals import pre_save, pre_delete
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
+from django.dispatch import receiver
 
 from basketapp.models import Basket
 from ordersapp.forms import OrderItemEditForm
@@ -109,3 +111,20 @@ def forming_complete(request, pk):
     order.save()
 
     return HttpResponseRedirect(reverse('order:list'))
+
+
+@receiver(pre_save, sender=Basket)
+@receiver(pre_save, sender=OrderItem)
+def product_quantity_update_save(sender, update_fields, instance, **kwargs):
+    if instance.pk:
+        instance.product.quantity -= instance.quantity - instance.get_item(instance.pk).quantity
+    else:
+        instance.product.quantity -= instance.quantity
+    instance.product.save()
+
+
+@receiver(pre_delete, sender=Basket)
+@receiver(pre_delete, sender=OrderItem)
+def product_quantity_update_delete(sender, instance, **kwargs):
+    instance.product.quantity += instance.quantity
+    instance.product.save()
