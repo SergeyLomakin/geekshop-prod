40984e543be2b07514dc6829bbffa1bbaf46f7fd
SergeyLomakin <sergeylomakinals@mail.ru>
2021-07-07 19:53:51 +0500

    created orders_scripts.js

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -43,7 +43,7 @@ class OrderCreate(CreateView):
                     form.initial['product'] = basket_items[num].product
                     form.initial['quantity'] = basket_items[num].quantity
                     form.initial['price'] = basket_items[num].product.price
-                basket_items.delete()
+                # basket_items.delete()
             else:
                 formset = OrderFormSet()
         data['orderitems'] = formset
