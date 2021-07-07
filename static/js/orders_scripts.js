66b052a49b1fbe97af9443921cd7bdb90050c1fa
SergeyLomakin <sergeylomakinals@mail.ru>
2021-07-07 21:08:38 +0500

    django-dynamic-formset

--- static/js/orders_scripts.js
+++ static/js/orders_scripts.js
@@ -51,4 +51,19 @@ window.onload = function () {
         $('.order_total_quantity').text(order_total_quantity.toString());
         $('.order_total_cost').text(order_total_price.toString());
     }
+
+    $('.formset_row').formset({
+        addText: 'Добавить продукт',
+        deleteText: 'Удалить',
+        prefix: 'orderitems',
+        remote: deleteOrderItem,
+    });
+
+    function deleteOrderItem(row) {
+        var target_name = row[0].querySelector('input[type=number]').name;
+        orderitem_num = parseInt(target_name.replace('orderitems-', '').replace('-quantity'));
+
+        delta_quantity = -quantity_arr[orderitem_num];
+        orderSummaryUpdate(price_arr[orderitem_num], delta_quantity);
+    }
 };
