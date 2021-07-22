5ee4dcfcb004c04efb22a42fc6ec4671bfd2e589
SergeyLomakin <sergeylomakinals@mail.ru>
2021-07-22 22:28:06 +0500

    F object

--- basketapp/views.py
+++ basketapp/views.py
@@ -3,6 +3,7 @@ from basketapp.models import Basket
 from mainapp.models import Product
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
+from django.db.models import F
 
 from django.template.loader import render_to_string
 from django.http import JsonResponse
@@ -31,7 +32,8 @@ def basket_add(request, pk):
     old_basket_item = Basket.objects.filter(user=request.user, product=product)
     
     if old_basket_item:
-        old_basket_item[0].quantity += 1
+        # old_basket_item[0].quantity += 1
+        old_basket_item[0].quantity = F('quantity') + 1
         old_basket_item[0].save()
     else:
         new_basket_item = Basket(user=request.user, product=product)
