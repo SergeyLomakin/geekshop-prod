567fd07b5b9fb63740ac22d3de24e30fa05b5bde
SergeyLomakin <sergeylomakinals@mail.ru>
2021-07-24 14:20:30 +0500

    create unit test TestMainSmokeTest

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.test.client import Client
 
-# Create your tests here.
+from mainapp.models import ProductCategory, Product
+
+
+class TestMainSmokeTest(TestCase):
+    status_code_success = 200
+
+    def SetUp(self):
+        cat_1 = ProductCategory.objects.create(
+            name='cat 1'
+        )
+        Product.objects.create(
+            category=cat_1,
+            name='prod 1'
+        )
+        self.client = Client()
+
+    def test_main_app_urls(self):
+        responce = self.client.get('/')
+        self.assertEqual(responce.status_code, self.status_code_success)
